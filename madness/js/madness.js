935f65da26a82e191d28e42c54ea3cbc9c0a06b0
Kenny Stepney <kennystepney@hotmail.com>
2015-04-16 10:00:23 -0500

    Update madness.js

--- madness/js/madness.js
+++ madness/js/madness.js
@@ -1,2 +1,6 @@
 
 /** might make more sense to split javascript between multiple files for each page */
+//Add a window/tab closed listener. Probably to log out the user
+window.onbeforeunload = function(){
+  
+}
