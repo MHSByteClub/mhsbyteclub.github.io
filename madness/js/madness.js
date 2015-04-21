fd47f087d9410072a74aafa9f7c67fcc00ed1557
Kenny Stepney <kennystepney@hotmail.com>
2015-04-21 10:01:45 -0500

    Update madness.js

--- madness/js/madness.js
+++ madness/js/madness.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $("#click_mad").click(function(){
     $(this).fadeOut('slow');
   });
+  $(".pass_by").mouseover(function(){
+    
+  });
 });
 /** might make more sense to split javascript between multiple files for each page */
 //Add a window/tab closed listener. Probably to log out the user
