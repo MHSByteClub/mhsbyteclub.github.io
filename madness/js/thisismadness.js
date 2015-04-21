a18ce72890b06b0ca3fd388ce42574d7439f8432
Kenny Stepney <kennystepney@hotmail.com>
2015-04-21 08:34:14 -0500

    added a script to make login prettier

--- madness/js/thisismadness.js
+++ madness/js/thisismadness.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-  madness();
+  init_madness();
 });
 
-function madness(){
-  var canvas = document.getElementById("mad");
-  var mad_text = [];
+function init_madness(){
+  var canvas = document.getElementById("mad"),
+  ctx = canvas.getContext("2d"),
+  letters = [],
+  phrases = ["Login", "March Madness", "Rock", "Paper", "Scissor"];
+  
+  for(var i = 97; i < 122; i++){
+    letters.push(String.fromCharCode(i));
+  }
+  
+  if(Math.floor(Math.random(0, 2)) == 1){
+    use_phrases(phrases);
+  }else{
+    use_letters(letters);
+  }
+}
+
+function use_phrases(text){
+  
+}
+
+function use_letters(text){
+  
 }
