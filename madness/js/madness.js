3359ff106f0ced34708be8134f9c85a2c2e073ea
Kenny Stepney <kennystepney@hotmail.com>
2015-04-21 10:07:45 -0500

    start to implement some easter eggs

--- madness/js/madness.js
+++ madness/js/madness.js
@@ -1,14 +1,15 @@
+var key_combo = [];
 $(document).ready(function(){
-  var key_combo = [];
   $("#click_mad").click(function(){
     $(this).fadeOut('slow');
   });
   $(".pass_by").mouseover(function(){
     
   });
-  $(".page").keydown(function(){
-    key_combo.push(event.keyCode);
-  });
+});
+
+window.addEventListener("keydown", function(event){
+  key_combo.push(event.keyCode);
 });
 /** might make more sense to split javascript between multiple files for each page */
 //Add a window/tab closed listener. Probably to log out the user
