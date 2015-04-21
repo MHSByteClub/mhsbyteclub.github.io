0c49adda7b91c98060578fb3d596444dedfa4f67
Kenny Stepney <kennystepney@hotmail.com>
2015-04-21 09:45:31 -0500

    Update thisismadness.js

--- madness/js/thisismadness.js
+++ madness/js/thisismadness.js
@@ -6,7 +6,8 @@ function init_madness(){
   var canvas = document.getElementById("mad"),
   ctx = canvas.getContext("2d"),
   letters = [],
-  phrases = ["Login", "March Madness", "Rock", "Paper", "Scissor"];
+  phrases = ["Login", "March Madness", "Rock", "Paper", "Scissor"],
+  lim = Math.floor(Math.random(0, 100));
   
   for(var i = 97; i < 122; i++){
     letters.push(String.fromCharCode(i));
@@ -23,9 +24,17 @@ function init_madness(){
   
   function use_phrases(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for(var i = 0; i < 100; i++){
+      ctx.fillRect(Math.random(0, canvas.width), Math.random(0, canvas.height), Math.random(10, 100), Math.random(10, 100));
+    }
   }
   
   function use_letters(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for(var i = 0; i < 100; i++){
+      ctx.fillRect(Math.random(0, canvas.width), Math.random(0, canvas.height), Math.random(10, 100), Math.random(10, 100));
+    }
   }
 }
