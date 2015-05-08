1304020d95a59c1ed22e6f333034c8bf8b5d0a07
name/name <kennystepney@hotmail.com>
2015-05-08 08:29:53 -0500

    fixed some syntax

--- madness/game.dart
+++ madness/game.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 
 void main(){
   CanvasElement canvas = querySelector('#madgame');
-  CanvasRenderingContext2D ctx = canvas.context2D;
+  CanvasRenderingContext2D ctx = canvas.getContext("2d");
 
   ctx.beginPath();
   ctx.arc(75, 75, 10, 0, Math.PI*2, true); 
