d67443450d0b0d2e323a2b8fcdf3f4b8ee440d14
name/name <kennystepney@hotmail.com>
2015-05-07 20:36:35 -0500

    improved the dart code

--- madness/game.dart
+++ madness/game.dart
@@ -1,5 +1,7 @@
+import 'dart:html';
+
 void main(){
-  CanvasElement canvas = querySelector('canvas');
+  CanvasElement canvas = document.querySelector('#madgame');
   CanvasRenderingContext2D ctx = canvas.context2D;
   
   ctx.fillRect(0,0,300,150);
