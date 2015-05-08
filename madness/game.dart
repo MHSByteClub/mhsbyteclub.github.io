900ba2211e36c3aa5098bb91dbf2bb34206bf848
name/name <kennystepney@hotmail.com>
2015-05-08 07:57:34 -0500

    fixed an error

--- madness/game.dart
+++ madness/game.dart
@@ -1,9 +1,9 @@
 import 'dart:html';
 
 void main(){
-  CanvasElement canvas = document.querySelector('#madgame');
+  CanvasElement canvas = querySelector('#madgame');
   CanvasRenderingContext2D ctx = canvas.context2D;
-  
+
   ctx.beginPath();
   ctx.arc(75, 75, 10, 0, Math.PI*2, true); 
   ctx.closePath();
