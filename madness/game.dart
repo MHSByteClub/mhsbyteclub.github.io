cba5fb4aa1cad774a4ae7dbe58ebc4033ce1dc70
name/name <kennystepney@hotmail.com>
2015-05-11 12:13:21 -0500

    Update game.dart

--- madness/game.dart
+++ madness/game.dart
@@ -7,9 +7,19 @@ void main(){
 
 class GameMain{
   CanvasElement canvas;
+  
   GameMain(this.canvas);
   
-  void start(){
+  start(){
+    var content = canvas.context2D;
     
+     context..lineWidth = 0.5
+           ..fillStyle = "red";
+  
+    context..beginPath()
+           ..arc(p.x, p.y, bodySize, 0, PI * 2, false)
+           ..fill()
+           ..closePath()
+           ..stroke();
   }
 }
