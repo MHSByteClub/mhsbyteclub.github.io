d0be8898263d2fae30aeac0d21e89d5a7961f5be
Kenny Stepney <kennystepney@hotmail.com>
2015-09-02 13:34:01 -0500

    Update express.js

--- madness/express.js
+++ madness/express.js
@@ -1,3 +1,15 @@
 //Request client port, 4004 if none.
 var gameport = process.env.PORT || 4004, 
     io = require('socket.io'),
+    express = require('express'),
+    UUID = require('node-uuid'),
+    verbose = false,
+    app = express.createServer();
+    
+    app.listen(gameport);
+    
+    alert('\t :: Express :: Listening on port '+gameport);
+    
+    app.get('/', function(req, res)){
+        //res.sendfile(__dirname + '/simplest.html');
+    });
