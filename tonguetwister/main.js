14a6842caf7476d170bc6a2a4b785316c4ed8887
Kenny Stepney <kennystepney@hotmail.com>
2015-10-01 15:45:09 -0500

    Added certain variable

--- tonguetwister/main.js
+++ tonguetwister/main.js
@@ -1,111 +1,112 @@
 function generate() {
     var number = random(0,26);
+    var div = document.getElementById('tongue_twister');
 
     if(number<1){
         for(var forA=1; forA<8; forA++){
-            text(words.a[floor(random(0,words.a.length))],10,forA*50);
+            div.text(words.a[floor(random(0,words.a.length))],10,forA*50);
         }
     }
     else if(number<2){
         for(var forB=1; forB<8; forB++){
-            text(words.b[floor(random(0,words.b.length))],10,forB*50);
+            div.text(words.b[floor(random(0,words.b.length))],10,forB*50);
         }
     }
     else if(number<3){
         for(var forC=1; forC<8; forC++){
-            text(words.c[floor(random(0,words.c.length))],10,forC*50);
+            div.text(words.c[floor(random(0,words.c.length))],10,forC*50);
         }
     }else if(number<4){
         for(var forD=1; forD<8; forD++){
-            text(words.d[floor(random(0,words.d.length))],10,forD*50);
+            div.text(words.d[floor(random(0,words.d.length))],10,forD*50);
         }
     }else if(number<5){
         for(var forE=1; forE<8; forE++){
-            text(words.e[floor(random(0,words.e.length))],10,forE*50);
+            div.text(words.e[floor(random(0,words.e.length))],10,forE*50);
         }
     }else if(number<6){
         for(var forF=1; forF<8; forF++){
-            text(words.f[floor(random(0,words.f.length))],10,forF*50);
+            div.text(words.f[floor(random(0,words.f.length))],10,forF*50);
         }
     }else if(number<7){
         for(var forG=1; forG<8; forG++){
-            text(words.g[floor(random(0,words.g.length))],10,forG*50);
+            div.text(words.g[floor(random(0,words.g.length))],10,forG*50);
         }
     }else if(number<8){
         for(var forH=1; forH<8; forH++){
-            text(words.h[floor(random(0,words.h.length))],10,forH*50);
+            div.text(words.h[floor(random(0,words.h.length))],10,forH*50);
         }
     }else if(number<9){
         for(var forI=1; forI<8; forI++){
-            text(words.i[floor(random(0,words.i.length))],10,forI*50);
+            div.text(words.i[floor(random(0,words.i.length))],10,forI*50);
         }
     }else if(number<10){
         for(var forJ=1; forJ<8; forJ++){
-            text(words.j[floor(random(0,words.j.length))],10,forJ*50);
+            div.text(words.j[floor(random(0,words.j.length))],10,forJ*50);
         }
     }else if(number<11){
         for(var forK=1; forK<8; forK++){
-            text(words.k[floor(random(0,words.k.length))],10,forK*50);
+            div.text(words.k[floor(random(0,words.k.length))],10,forK*50);
         }
     }else if(number<12){
         for(var forL=1; forL<8; forL++){
-            text(words.l[floor(random(0,words.l.length))],10,forL*50);
+            div.text(words.l[floor(random(0,words.l.length))],10,forL*50);
         }
     }else if(number<13){
         for(var forM=1; forM<8; forM++){
-            text(words.m[floor(random(0,words.m.length))],10,forM*50);
+            div.text(words.m[floor(random(0,words.m.length))],10,forM*50);
         }
     }else if(number<14){
         for(var forN=1; forN<8; forN++){
-            text(words.n[floor(random(0,words.n.length))],10,forN*50);
+            div.text(words.n[floor(random(0,words.n.length))],10,forN*50);
         }
     }else if(number<15){
         for(var forO=1; forO<8; forO++){
-            text(words.o[floor(random(0,words.o.length))],10,forO*50);
+            div.text(words.o[floor(random(0,words.o.length))],10,forO*50);
         }
     }else if(number<16){
         for(var forP=1; forP<8; forP++){
-            text(words.p[floor(random(0,words.p.length))],10,forP*50);
+            div.text(words.p[floor(random(0,words.p.length))],10,forP*50);
         }
     }else if(number<17){
         for(var forQ=1; forQ<8; forQ++){
-            text(words.q[floor(random(0,words.q.length))],10,forQ*50);
+            div.text(words.q[floor(random(0,words.q.length))],10,forQ*50);
         }
     }else if(number<18){
         for(var forR=1; forR<8; forR++){
-            text(words.r[floor(random(0,words.r.length))],10,forR*50);
+            div.text(words.r[floor(random(0,words.r.length))],10,forR*50);
         }
     }else if(number<19){
         for(var forS=1; forS<8; forS++){
-            text(words.s[floor(random(0,words.s.length))],10,forS*50);
+            div.text(words.s[floor(random(0,words.s.length))],10,forS*50);
         }
     }else if(number<20){
         for(var forT=1; forT<8; forT++){
-            text(words.t[floor(random(0,words.t.length))],10,forT*50);
+            div.text(words.t[floor(random(0,words.t.length))],10,forT*50);
         }
     }else if(number<21){
         for(var forU=1; forU<8; forU++){
-            text(words.u[floor(random(0,words.u.length))],10,forU*50);
+            div.text(words.u[floor(random(0,words.u.length))],10,forU*50);
         }
     }else if(number<22){
         for(var forV=1; forV<8; forV++){
-            text(words.v[floor(random(0,words.v.length))],10,forV*50);
+            div.text(words.v[floor(random(0,words.v.length))],10,forV*50);
         }
     }else if(number<23){
         for(var forW=1; forW<8; forW++){
-            text(words.w[floor(random(0,words.w.length))],10,forW*50);
+            div.text(words.w[floor(random(0,words.w.length))],10,forW*50);
         }
     }else if(number<24){
         for(var forX=1; forX<8; forX++){
-            text(words.x[floor(random(0,words.x.length))],10,forX*50);
+            div.text(words.x[floor(random(0,words.x.length))],10,forX*50);
         }
     }else if(number<25){
         for(var forY=1; forY<8; forY++){
-            text(words.y[floor(random(0,words.y.length))],10,forY*50);
+            div.text(words.y[floor(random(0,words.y.length))],10,forY*50);
         }
     }else {
         for(var forZ=1; forZ<8; forZ++){
-            text(words.z[floor(random(0,words.z.length))],10,forZ*50);
+            div.text(words.z[floor(random(0,words.z.length))],10,forZ*50);
         }
     }
 };
