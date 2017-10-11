e9cad858b02dce1a7c98490b2b02150d8da596e1
mrfarmer7771 <mfarmer@d120.org>
2017-10-11 21:18:10 +0000

    having button fun

---
+++ js/buttonFun.js
@@ -0,0 +1,6 @@
+var buttonFun = function (){
+    var outputHolder = document.getElementById('funOutput');
+    outputHolder.innerHTML('This is a string we put in here');
+    console.log('THis worked!');
+    
+}
